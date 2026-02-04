6d35a6b84e4cf37ad4d4359ec9e0d257ab8ef40e
rgablejr <rgablejr@gmail.com>
2026-02-03 19:14:05 -0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+/**
+ * Adds two numbers.
+ * @customfunction
+ * @param {number} first First number.
+ * @param {number} second Second number.
+ * @returns {number} The sum of the two numbers.
+ */
+function add(first, second) {
+  return first + second;
+}
+
+// Essential: This initializes the functions
+CustomFunctions.associate("ADD", add);
